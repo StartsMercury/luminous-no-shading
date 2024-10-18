10c2e89fe42e48368e558a40fe9ff3600ec24590
StartsMercury <89975834+StartsMercury@users.noreply.github.com>
2024-10-18 11:00:21 +0800

    Update to Minecraft 1.21.2 Pre-Release 2

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ object Constants {
     const val VERSION = "0.2.2"
 
     const val VERSION_JAVA = 21
-    const val VERSION_MINECRAFT = "1.21.2-pre1"
+    const val VERSION_MINECRAFT = "1.21.2-pre2"
 }
 
 plugins {
@@ -54,7 +54,7 @@ tasks {
         val data = mapOf(
             "version" to Constants.VERSION,
             "version_java" to Constants.VERSION_JAVA,
-            "version_minecraft" to "1.21.2-beta.1",
+            "version_minecraft" to "1.21.2-beta.2",
         )
 
         inputs.properties(data)
