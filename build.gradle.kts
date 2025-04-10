f0328704e7ef79a6b9d92927bdc390ad0bc9a9a3
StartsMercury <89975834+StartsMercury@users.noreply.github.com>
2025-04-11 00:02:54 +0800

    Update Iris to 1.8.11

--- build.gradle.kts
+++ build.gradle.kts
@@ -91,7 +91,7 @@ loom {
     }
 }
 run {
-    val iris = "maven.modrinth:iris:1.8.0-beta.4+1.21-fabric"
+    val iris = "maven.modrinth:iris:1.8.11+1.21.5-fabric"
     val sodium = "maven.modrinth:sodium:mc1.21.5-0.6.13-fabric"
 
     createCompatTest("iris", iris, sodium)
