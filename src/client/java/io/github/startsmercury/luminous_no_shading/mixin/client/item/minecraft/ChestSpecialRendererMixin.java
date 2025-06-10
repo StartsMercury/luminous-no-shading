a03e164c5581e92ff21b1fb293d55f6f573b94c0
StartsMercury <89975834+StartsMercury@users.noreply.github.com>
2025-06-10 10:59:54 +0800

    Fix ender chest support
    (cherry picked from commit ebce6c6d2e376f67c2fd1b8a2e80ef72b4b4f6ac)

---
+++ src/client/java/io/github/startsmercury/luminous_no_shading/mixin/client/item/minecraft/ChestSpecialRendererMixin.java
@@ -0,0 +1,69 @@
+package io.github.startsmercury.luminous_no_shading.mixin.client.item.minecraft;
+
+import com.llamalad7.mixinextras.sugar.Local;
+import io.github.startsmercury.luminous_no_shading.impl.client.ClearRenderType;
+import io.github.startsmercury.luminous_no_shading.impl.client.LuminousNoShadingImpl;
+import io.github.startsmercury.luminous_no_shading.impl.client.NoShadingRenderTypes;
+import net.minecraft.client.renderer.special.ChestSpecialRenderer;
+import net.minecraft.client.resources.model.Material;
+import net.minecraft.world.item.ItemDisplayContext;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(ChestSpecialRenderer.class)
+public class ChestSpecialRendererMixin {
+    @Final
+    @Shadow
+    private Material material;
+
+    @Unique
+    private boolean ender;
+
+    @Unique
+    private boolean wasGui;
+
+    @Inject(method = "<init>", at = @At("RETURN"))
+    private void initEnder(final CallbackInfo callback) {
+        this.ender = this.material.texture().getPath().contains("ender");
+    }
+
+    @Inject(
+        method = "render",
+        at = @At(
+            value = "INVOKE",
+            target = """
+                Lnet/minecraft/client/resources/model/Material;       \
+                buffer(                                               \
+                    Lnet/minecraft/client/renderer/MultiBufferSource; \
+                    Ljava/util/function/Function;                     \
+                ) Lcom/mojang/blaze3d/vertex/VertexConsumer;          \
+            """
+        )
+    )
+    private void freeRenderType(
+        final CallbackInfo callback,
+        final @Local(argsOnly = true, ordinal = 0) ItemDisplayContext itemDisplayContext
+    ) {
+        if (!this.ender) {
+            return;
+        }
+
+        if (this.wasGui) {
+            if (itemDisplayContext != ItemDisplayContext.GUI && LuminousNoShadingImpl.isGuiOnly()) {
+                ClearRenderType.clear(this.material);
+                this.wasGui = false;
+            }
+        } else {
+            if (itemDisplayContext == ItemDisplayContext.GUI) {
+                ClearRenderType.clear(this.material);
+                this.material.renderType(NoShadingRenderTypes::entitySolid);
+                this.wasGui = true;
+            }
+        }
+    }
+}
