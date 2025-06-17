2a6789ce4bac9f3c65af168476b9b0c1a52ad731
StartsMercury <89975834+StartsMercury@users.noreply.github.com>
2025-06-18 07:36:38 +0800

    Update Sodium to 0.6.13 for Minecraft 1.21.6

--- build.gradle.kts
+++ build.gradle.kts
@@ -92,7 +92,7 @@ loom {
 }
 run {
     val iris = "maven.modrinth:iris:1.8.11+1.21.5-fabric"
-    val sodium = "maven.modrinth:sodium:mc1.21.5-0.6.13-fabric"
+    val sodium = "maven.modrinth:sodium:mc1.21.6-0.6.13-fabric"
 
     createCompatTest("iris", iris, sodium)
     createCompatTest("sodium", sodium)
