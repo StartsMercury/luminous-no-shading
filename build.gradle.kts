d0ff835571c9b344cc12209fde4add0d4d757ce0
StartsMercury <89975834+StartsMercury@users.noreply.github.com>
2025-08-30 06:51:57 +0800

    Update to Minecraft 1.21.7 Release Candidate 2

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ object Constants {
     const val VERSION = "0.2.3"
 
     const val VERSION_JAVA = 21
-    const val VERSION_MINECRAFT = "1.21.7-rc1"
+    const val VERSION_MINECRAFT = "1.21.7-rc2"
 }
 
 plugins {
@@ -54,7 +54,7 @@ tasks {
         val data = mapOf(
             "version" to Constants.VERSION,
             "version_java" to Constants.VERSION_JAVA,
-            "version_minecraft" to "1.21.7-rc.1",
+            "version_minecraft" to "1.21.7-rc.2",
         )
 
         inputs.properties(data)
